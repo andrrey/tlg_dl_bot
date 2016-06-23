6f47ef09db25f1d5e2647c0d587c62c9058610df
Andrey Semenyuk <andygluk@ya.ru>
2016-06-24 02:56:52 +0300

    ...

--- bot.py
+++ bot.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3.4
 
 from twx.botapi import TelegramBot
+from time import sleep
 import configparser
+
+sleep_time = 1
+
 config = configparser.ConfigParser()
 config.read("config.cfg")
-
 bot = TelegramBot(config.get('Config', 'Token'))
 bot.update_bot_info().wait()
 print(bot.username)
 
+offset=None
+
 while True:
-	updates = bot.get_updates().wait()
+	updates = bot.get_updates(offset).wait()
 	for update in updates:
 	    print(update)
+	    offset=update.update_id + 1
+	sleep(sleep_time)
