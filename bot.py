5095c24cffd85e6ce821bb8d11663c488f8eeaf7
Andrey Semenyuk <andygluk@ya.ru>
2016-06-24 03:08:37 +0300

    ...

--- bot.py
+++ bot.py
@@ -17,6 +17,15 @@ offset=None
 while True:
 	updates = bot.get_updates(offset).wait()
 	for update in updates:
-	    print(update)
+	    #print(update)
 	    offset=update.update_id + 1
+	    msg = update.message
+	    if msg is not None:
+	    	fromuser = msg.from
+	    	txt = msg.text.decode("utf-8")
+	    	username = fromuser.first_name.decode("utf-8")
+	    	if fromuser.last_name is not None:
+	    		username += fromuser.last_name.decode("utf-8")
+	    	print('From: ' + username + ': ' + txt)	    	
+
 	sleep(sleep_time)
