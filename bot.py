eba3d9228e8e4b34f26763a42edd134b47ae1f6e
Andrey Semenyuk <andygluk@ya.ru>
2016-06-26 13:57:54 +0300

    ...

--- bot.py
+++ bot.py
@@ -3,14 +3,30 @@
 from twx.botapi import TelegramBot
 from time import sleep
 import configparser
+import _mysql
 
 sleep_time = 1
 
 config = configparser.ConfigParser()
 config.read("config.cfg")
+db_host = config.get('DB', 'host')
+db_user = config.get('DB', 'user')
+db_pass = config.get('DB', 'password')
+db_database = 'telegrambot'
+botmaster = config.get('Config', 'botmaster')
+db_conn = _mysql.connect(host=db_host, user=db_user, passwd=db_pass, db=db_database)
 bot = TelegramBot(config.get('Config', 'Token'))
 bot.update_bot_info().wait()
-print(bot.username)
+print(bot.username + 'is up and running')
+
+
+def parse_command(command):
+	return False
+
+
+def parse_scene(scene, id):
+	return 'я тупой бот :('
+
 
 offset=None
 
@@ -24,11 +40,13 @@ while True:
 	    	fromuser = msg.sender
 	    	txt = msg.text
 	    	username = fromuser.first_name
+	    	userid = fromuser.id
 	    	if fromuser.last_name is not None:
-	    		username += fromuser.last_name
-	    	print('From ' + username + ': ' + txt)
-	    	if txt == 'obey':
-	    		bot.send_message(msg.chat.id, "хуюшки")
-	    	else:
-	    		bot.send_message(msg.chat.id, "я тупой бот")
+	    		username += ' ' + fromuser.last_name
+	    	print('From ' + username + txt + ' (id ' + userid + '): ' + txt)
+	    	if(userid == botmaster):
+	    		if(parse_command(txt)):
+	    			continue
+	    		else:
+	    			bot.send_message(msg.chat.id, parse_scene(txt, userid))
 	sleep(sleep_time)
